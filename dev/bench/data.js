9498b2c7b27b9cd90db12a159d2fa9ed199c577a
github-action-benchmark <github@users.noreply.github.com>
2024-04-09 03:10:26 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 31303e01335e969965e930147178e53fc7dc59f4

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1712632226839,
+  "repoUrl": "https://github.com/blockiosaurus/mpl-core",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "blockiosaurus@gmail.com",
+            "name": "blockiosaurus",
+            "username": "blockiosaurus"
+          },
+          "committer": {
+            "email": "blockiosaurus@gmail.com",
+            "name": "blockiosaurus",
+            "username": "blockiosaurus"
+          },
+          "distinct": true,
+          "id": "31303e01335e969965e930147178e53fc7dc59f4",
+          "message": "Updating ignore.",
+          "timestamp": "2024-04-08T23:06:38-04:00",
+          "tree_id": "f5fbd30ffe366b88328250fbc5617bf315e44525",
+          "url": "https://github.com/blockiosaurus/mpl-core/commit/31303e01335e969965e930147178e53fc7dc59f4"
+        },
+        "date": 1712632226346,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "create a new, empty asset",
+            "value": 7404,
+            "unit": "Compute Units"
+          }
+        ]
+      }
+    ]
+  }
+}
