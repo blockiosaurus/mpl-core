9032099c29aea55ae82e0b7f448cc9be1ce8ab09
github-action-benchmark <github@users.noreply.github.com>
2024-04-09 13:50:57 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 45fb4c6323d938217cb486e876f4f719c9b7dd21

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712632226839,
+  "lastUpdate": 1712670657615,
   "repoUrl": "https://github.com/blockiosaurus/mpl-core",
   "entries": {
     "Benchmark": [
@@ -28,6 +28,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "create a new, empty asset",
             "value": 7404,
+            "unit": "Compute Units"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "90809591+blockiosaurus@users.noreply.github.com",
+            "name": "blockiosaurus",
+            "username": "blockiosaurus"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "45fb4c6323d938217cb486e876f4f719c9b7dd21",
+          "message": "Merge branch 'metaplex-foundation:main' into main",
+          "timestamp": "2024-04-09T09:46:43-04:00",
+          "tree_id": "e00f474aa7a327cb043f0d3f910a59b163a5c0c0",
+          "url": "https://github.com/blockiosaurus/mpl-core/commit/45fb4c6323d938217cb486e876f4f719c9b7dd21"
+        },
+        "date": 1712670657108,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "create a new, empty asset",
+            "value": 7413,
             "unit": "Compute Units"
           }
         ]
