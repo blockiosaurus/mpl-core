0b86b9d4519cc2ff43ba79071b237eae76b9f326
github-action-benchmark <github@users.noreply.github.com>
2024-04-10 16:08:58 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 6f570ae889dfc7614ad18411920bb2db78a70eee

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712764876557,
+  "lastUpdate": 1712765338077,
   "repoUrl": "https://github.com/blockiosaurus/mpl-core",
   "entries": {
     "Benchmark": [
@@ -134,6 +134,50 @@ window.BENCHMARK_DATA = {
           },
           {
             "name": "create a new, empty asset with empty collection",
+            "value": 0,
+            "unit": "Bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "blockiosaurus@gmail.com",
+            "name": "blockiosaurus",
+            "username": "blockiosaurus"
+          },
+          "committer": {
+            "email": "blockiosaurus@gmail.com",
+            "name": "blockiosaurus",
+            "username": "blockiosaurus"
+          },
+          "distinct": true,
+          "id": "6f570ae889dfc7614ad18411920bb2db78a70eee",
+          "message": "Fixing titles.",
+          "timestamp": "2024-04-10T12:04:19-04:00",
+          "tree_id": "e3fcc0a165b51164ebb627ba0c15010d0a9288ee",
+          "url": "https://github.com/blockiosaurus/mpl-core/commit/6f570ae889dfc7614ad18411920bb2db78a70eee"
+        },
+        "date": 1712765336842,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CU: create a new, empty asset",
+            "value": 7404,
+            "unit": "Compute Units"
+          },
+          {
+            "name": "Space: create a new, empty asset",
+            "value": 91,
+            "unit": "Bytes"
+          },
+          {
+            "name": "CU: create a new, empty asset with empty collection",
+            "value": 0,
+            "unit": "Compute Units"
+          },
+          {
+            "name": "Space: create a new, empty asset with empty collection",
             "value": 0,
             "unit": "Bytes"
           }
